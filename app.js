d50e87923ce35defeffd07c6aa2a292363cdd44a
idrazovic <idrazovic@gmail.com>
2024-05-21 14:36:38 +0200

    body-parser added

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('middleware');
-    res.send('<h1>Express</h1>')
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add</button></form>')
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Express Home page</h1>')
 });
 
 app.listen(8000);
