d10a607672bb3d8be0be255b96e29ecd6eb8dd78
idrazovic <idrazovic@gmail.com>
2024-05-29 21:56:54 +0200

    mongodb added

--- util/database.js
+++ util/database.js
@@ -1,8 +1,26 @@
-const { Sequelize } = require('sequelize');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@nodejs-shop.os5pfbg.mongodb.net/?retryWrites=true&w=majority&appName=nodejs-shop`;
 
-const sequelize = new Sequelize('nodejs', 'root', 'rootroot', {
-    host: 'localhost',
-    dialect: 'mysql'
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-module.exports = sequelize;
+async function mongoConnect() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+module.exports = mongoConnect;
